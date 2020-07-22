20c59db1eb3903cbd6b638d4e8c158bd28c163f3
ifer-itcast <yangkangkang@itcast.cn>
2020-07-22 22:39:08 +0800

    07_后端数据校验和前端错误提示

---
+++ src/pages/register/RegisterForm.jsx
@@ -0,0 +1,112 @@
+import React, { Component } from "react";
+import classnames from "classnames";
+export default class RegisterForm extends Component {
+  state = {
+    userInfo: {
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+    errMsg: [],
+  };
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ errMsg: [] });
+    const { data } = await this.props.registerFn.registerAc(
+      this.state.userInfo
+    );
+    if (data.status === 1) {
+      this.setState({
+        errMsg: data.msg,
+      });
+    }
+  };
+  handleChange = e => {
+    this.setState({
+      userInfo: {
+        ...this.state.userInfo,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+  render() {
+    const { username, email, password, passwordConfirm } = this.state.userInfo;
+    const { errMsg } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {/* Username */}
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            className={classnames("form-control", {
+              "is-invalid": errMsg[0] === "username",
+            })}
+            id="username"
+            name="username"
+            defaultValue={username}
+            onChange={this.handleChange}
+          />
+          <small className="form-text text-muted">
+            {errMsg[0] === "username" && errMsg[1]}
+          </small>
+        </div>
+        {/* Email */}
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className={classnames("form-control", {
+              "is-invalid": errMsg[0] === "email",
+            })}
+            id="email"
+            name="email"
+            defaultValue={email}
+            onChange={this.handleChange}
+          />
+          <small className="form-text text-muted">
+            {errMsg[0] === "email" && errMsg[1]}
+          </small>
+        </div>
+        {/* password */}
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className={classnames("form-control", {
+              "is-invalid": errMsg[0] === "password",
+            })}
+            id="password"
+            name="password"
+            defaultValue={password}
+            onChange={this.handleChange}
+          />
+          <small className="form-text text-muted">
+            {errMsg[0] === "password" && errMsg[1]}
+          </small>
+        </div>
+        {/* passwordConfirm */}
+        <div className="form-group">
+          <label htmlFor="passwordConfirm">passwordConfirm</label>
+          <input
+            type="password"
+            className={classnames("form-control", {
+              "is-invalid": errMsg[0] === "passwordConfirm",
+            })}
+            id="passwordConfirm"
+            name="passwordConfirm"
+            defaultValue={passwordConfirm}
+            onChange={this.handleChange}
+          />
+          <small className="form-text text-muted">
+            {errMsg[0] === "passwordConfirm" && errMsg[1]}
+          </small>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sign up
+        </button>
+      </form>
+    );
+  }
+}
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,94 +1,24 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { actionCrators as registerActionCreators } from './store';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCrators as registerActionCreators } from "./store";
+import RegisterForm from "./RegisterForm";
 
 class Register extends Component {
-  state = {
-    username: '',
-    email: '',
-    password: '',
-    passwordConfirm: ''
-  };
-  handleSubmit = async e => {
-    e.preventDefault();
-    const { data } = await this.props.registerFn.registerAc(this.state);
-    console.log(data);
-  };
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
   render() {
-    const { username, email, password, passwordConfirm } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        {/* Username */}
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            className="form-control"
-            id="username"
-            name="username"
-            defaultValue={username}
-            onChange={this.handleChange}
-          />
-        </div>
-        {/* Email */}
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            defaultValue={email}
-            onChange={this.handleChange}
-          />
-        </div>
-        {/* password */}
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            name="password"
-            defaultValue={password}
-            onChange={this.handleChange}
-          />
-        </div>
-        {/* passwordConfirm */}
-        <div className="form-group">
-          <label htmlFor="passwordConfirm">passwordConfirm</label>
-          <input
-            type="password"
-            className="form-control"
-            id="passwordConfirm"
-            name="passwordConfirm"
-            defaultValue={passwordConfirm}
-            onChange={this.handleChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Sign up
-        </button>
-      </form>
-    );
+    return <RegisterForm {...this.props} />;
   }
 }
 
 const mapStateToProps = state => {
   return {
-    registerData: state.register
+    registerData: state.register,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    registerFn: bindActionCreators(registerActionCreators, dispatch)
+    registerFn: bindActionCreators(registerActionCreators, dispatch),
   };
 };
 
