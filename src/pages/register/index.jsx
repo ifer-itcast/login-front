439b6f0e6589af77be9a0482f90a449baacec738
ifer-itcast <yangkangkang@itcast.cn>
2020-07-22 10:15:53 +0800

    04_收集表单数据

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 
 export default class Register extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: ''
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
   render() {
+    const { username, email, password, passwordConfirm } = this.state;
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         {/* Username */}
         <div className="form-group">
           <label htmlFor="username">Username</label>
           <input
-            type="email"
+            type="text"
             className="form-control"
             id="username"
+            name="username"
+            defaultValue={username}
+            onChange={this.handleChange}
           />
         </div>
         {/* Email */}
@@ -20,6 +39,9 @@ export default class Register extends Component {
             type="email"
             className="form-control"
             id="email"
+            name="email"
+            defaultValue={email}
+            onChange={this.handleChange}
           />
         </div>
         {/* password */}
@@ -29,6 +51,9 @@ export default class Register extends Component {
             type="password"
             className="form-control"
             id="password"
+            name="password"
+            defaultValue={password}
+            onChange={this.handleChange}
           />
         </div>
         {/* passwordConfirm */}
@@ -38,6 +63,9 @@ export default class Register extends Component {
             type="password"
             className="form-control"
             id="passwordConfirm"
+            name="passwordConfirm"
+            defaultValue={passwordConfirm}
+            onChange={this.handleChange}
           />
         </div>
         <button type="submit" className="btn btn-primary">
